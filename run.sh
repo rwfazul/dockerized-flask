76931181e4d93570c9d9ae1c85587c0b7a9109ee
rwfazul <rwfazul@inf.ufsm.br>
2019-05-23 17:39:06 -0300

    simple script to scale app

---
+++ run.sh
@@ -0,0 +1,8 @@
+docker-compose up --scale app=2
+
+# View logs: $ docker-compose logs -f -t 
+# Shutdown: $ docker-compose down 
+
+# Restart specific service/container: $ docker-compose restart <service_name> 
+# View logs of specific service/container: $ docker-compose logs -f -t <service_name>
+# ...
